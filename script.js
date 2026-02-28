3a0b6bac93c29f5281862d842370ba2614f45323
Amiel Joshua Penaflor <amielpenaflor123@gmail.com>
2026-02-28 10:53:29 +0800

    add clear and delete functions

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ function appendOperation(operation) {
 function operate() {
 
 }
+
+function clear() {
+  document.getElementById('display').value = '';
+}
+
+function delete (){
+  document.getElementById('display').value.slice(0, -1);
+}
